e14e7a99e411bc0081aadedd02efb6ff0543d0fa
AtomJump <peter@atomjump.com>
2016-10-21 11:37:22 +1300

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # nags_reporter
 Reports notifications live from Nagios monitoring and displays them on an AtomJump messaging forum
+
+
+In the file:
+/usr/local/nagios/etc/objects/commands.cfg
+
+you should see these command definitions:
+
+```
+# 'notify-host-by-email' command definition
+define command{
+	command_name	notify-host-by-email
+	command_line	/usr/bin/printf "%b" "***** Nagios *****\n\nNotification Type: $NOTIFICATIONTYPE$\nHost: $HOSTNAME$\nState: $HOSTSTATE$\nAddress: $HOSTADDRESS$\nInfo: $HOSTOUTPUT$\n\nDate/Time: $LONGDATETIME$\n" | /usr/sbin/sendmail -s "** $NOTIFICATIONTYPE$ Host Alert: $HOSTNAME$ is $HOSTSTATE$ **" $CONTACTEMAIL$
+	}
+
+# 'notify-service-by-email' command definition
+define command{
+	command_name	notify-service-by-email
+	command_line	/usr/bin/printf "%b" "***** Nagios *****\n\nNotification Type: $NOTIFICATIONTYPE$\n\nService: $SERVICEDESC$\nHost: $HOSTALIAS$\nAddress: $HOSTADDRESS$\nState: $SERVICESTATE$\n\nDate/Time: $LONGDATETIME$\n\nAdditional Info:\n\n$SERVICEOUTPUT$\n" | /usr/sbin/sendmail -s "** $NOTIFICATIONTYPE$ Service Alert: $HOSTALIAS$/$SERVICEDESC$ is $SERVICESTATE$ **" $CONTACTEMAIL$
+	}
+```
+
+Also see https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/3/en/notifications.html for further information.
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+	//Cron job to add new news RSS feeds every 5 minutes.
+	//To install put the following line in after typing 
+	//		
+	//		/usr/bin/php /your_server_path/plugins/nags_reporter/index.php "your message here"
+
+
+	if(!isset($nags_reporter_config)) {
+        //Get global plugin config - but only once
+		$data = file_get_contents (dirname(__FILE__) . "/config/config.json");
+        if($data) {
+            $nags_reporter_config = json_decode($data, true);
+            if(!isset($nags_reporter_config)) {
+                echo "Error: nags_reporter config/config.json is not valid JSON.";
+                exit(0);
+            }
+     
+        } else {
+            echo "Error: Missing config/config.json in nags_reporter plugin.";
+            exit(0);
+     
+        }
+  
+  
+    }
+
+
+
+ 	$start_path = $nags_reporter_config['serverPath'];
+
+	
+	
+	$notify = false;
+	include_once($start_path . 'config/db_connect.php');	
+	
+	$define_classes_path = $start_path;     //This flag ensures we have access to the typical classes, before the cls.pluginapi.php is included
+	require($start_path . "classes/cls.pluginapi.php");
+	
+	$api = new cls_plugin_api();
+	
+	
+	if($argc >= 1) {
+		$message = intval($argv[1]);
+	} else {
+		$message = "";
+	}
+	
+
+
+			  
+			  
+			  
+			  
+			  
+
+						
+
+	 $shouted = $message;		//guid may not be url for some feeds, may need to have link
+	 $your_name = $nags_reporter_config['name'];
+	 $whisper_to = "";		//to the whole forum
+	 $email = $nags_reporter_config['email'];
+	 $ip = "92.27.10.17"; //must be something anything
+	 $forum_name = $nags_reporter_config['forum'];
+
+
+ 	 //Get the forum id
+     $forum_info = $api->get_forum_id($forum_name);
+	
+	 //Send the message
+	 $api->new_message($your_name, $shouted, $whisper_to, $email, $ip, $forum_info['forum_id'], false);
+												
+				
+					
+			
+			  
+
+	session_destroy();  //remove session
+	
+	
+	
+	
+?>
